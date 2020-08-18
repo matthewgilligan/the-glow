7e75074a6b28a5c7d47869b9dfde02dfbefc075a
Matthew Gilligan <mcgilligan97@gmail.com>
2020-08-18 20:23:25 +0100

    Create basic header and footer

---
+++ src/components/header.js
@@ -0,0 +1,37 @@
+import React from "react"
+import { Link } from "gatsby"
+import { FaFacebookF, FaInstagram, FaTwitter } from 'react-icons/fa';
+
+const Header = () => {
+  return (
+    <div>
+      <ul>
+        <li>
+          <Link to="/news">News</Link>
+        </li>
+        <li>
+          <Link to="/reviews">Reviews</Link>
+        </li>
+        <li>
+          <Link to="/features">Features</Link>
+        </li>
+        <li>
+          <Link to="/guides">Guides</Link>
+        </li>
+      </ul>
+      <ul>
+        <li>
+          <a href="https://www.twitter.com/"><FaFacebookF /></a>
+        </li>
+        <li>
+          <a href="https://www.instagram.com/"><FaInstagram /></a>
+        </li>
+        <li>
+          <a href="https://www.facebook.com/"><FaTwitter /></a>
+        </li>
+      </ul>
+    </div>
+  )
+}
+
+export default Header
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,35 +1,13 @@
 import React from "react"
-import { Link } from "gatsby"
-import { FaFacebookF, FaInstagram, FaTwitter } from 'react-icons/fa';
+
+import Footer from "../components/footer"
+import Header from "../components/header"
 
 const IndexPage = () => {
   return (
     <div>
-      <ul>
-        <li>
-          <Link to="/news">News</Link>
-        </li>
-        <li>
-          <Link to="/reviews">Reviews</Link>
-        </li>
-        <li>
-          <Link to="/features">Features</Link>
-        </li>
-        <li>
-          <Link to="/guides">Guides</Link>
-        </li>
-      </ul>
-      <ul>
-        <li>
-          <a href="https://www.twitter.com/"><FaFacebookF /></a>
-        </li>
-        <li>
-          <a href="https://www.instagram.com/"><FaInstagram /></a>
-        </li>
-        <li>
-          <a href="https://www.facebook.com/"><FaTwitter /></a>
-        </li>
-      </ul>
+      <Header />
+      <Footer />
     </div>
   )
 }
