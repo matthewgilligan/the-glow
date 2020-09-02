e103c50592ac7a6e22a690800ac2841eb2eaf84a
Matthew Gilligan <mcgilligan97@gmail.com>
2020-09-02 17:44:40 +0100

    Add better styling to footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -7,16 +7,12 @@ import logo from "../images/red_logo_text_medium.png"
 
 const Footer = () => {
   return (
-    <div>
+    <footer className={footerStyles.footer}>
       <div className={footerStyles.logos}>
-        <ul className={footerStyles.socialList}>
-          <li>
-            <FaSpotify className={footerStyles.socialItem} href="https://www.spotify.com/" target="_blank" rel="noreferrer"/>
-          </li>
-          <li>
-            <FaApple className={footerStyles.socialItem} href="https://www.apple.com/" target="_blank" rel="noreferrer"/>
-          </li>
-        </ul>
+        <div className={footerStyles.copyright}>
+          <p>© 2020 The Glow JP Ltd. All rights reserved.</p>
+          <Link className={footerStyles.copyrightLinks} to="/privacy-policy">Privacy Policy</Link> | <Link className={footerStyles.copyrightLinks} to="/terms-of-service">Terms of Service</Link>
+        </div>
         <img src={logo} className={footerStyles.logo} alt="Logo" />
         <ul className={footerStyles.socialList}>
           <li>
@@ -30,6 +26,25 @@ const Footer = () => {
           </li>
         </ul>
       </div>
+        <ul className={footerStyles.socialMobileList}>
+          <li>
+            <FaInstagram className={footerStyles.socialMobileItem} href="https://www.instagram.com/" target="_blank" rel="noreferrer"/>
+          </li>
+          <li>
+            <FaFacebookF className={footerStyles.socialMobileItem} href="https://www.twitter.com/" target="_blank" rel="noreferrer"/>
+          </li>
+          <li>
+            <FaTwitter className={footerStyles.socialMobileItem} href="https://www.facebook.com/" target="_blank" rel="noreferrer"/>
+          </li>
+        </ul>
+        <ul className={footerStyles.socialMobileList}>
+          <li>
+            <FaSpotify className={footerStyles.socialMobileItem} href="https://www.spotify.com/" target="_blank" rel="noreferrer"/>
+          </li>
+          <li>
+            <FaApple className={footerStyles.socialMobileItem} href="https://www.apple.com/" target="_blank" rel="noreferrer"/>
+          </li>
+        </ul>
         <ul className={footerStyles.footerList}>
           <li>
             <Link className={footerStyles.footerItem} to="/about">About Us</Link>
@@ -47,11 +62,11 @@ const Footer = () => {
             <Link className={footerStyles.footerItem} to="/contact">Contact</Link>
           </li>
         </ul>
-        <div className={footerStyles.copyright}>
+        <div className={footerStyles.copyrightMobile}>
           <p>© 2020 The Glow JP Ltd. All rights reserved.</p>
-          <Link className={footerStyles.copyrightLinks} to="/privacy-policy">Privacy Policy</Link> | <Link className={footerStyles.copyrightLinks} to="/terms-of-service">Terms of Service</Link>
+          <Link className={footerStyles.copyrightMobileLinks} to="/privacy-policy">Privacy Policy</Link> | <Link className={footerStyles.copyrightMobileLinks} to="/terms-of-service">Terms of Service</Link>
         </div>
-    </div>
+    </footer>
   )
 }
 
