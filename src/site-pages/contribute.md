6b281a2df41cf8fd3790bdab77771ad29b2cfb20
Matthew Gilligan <mcgilligan97@gmail.com>
2020-09-10 14:28:41 +0100

    Connect markdown content to new pages

---
+++ src/site-pages/contribute.md
@@ -0,0 +1,6 @@
+---
+title: "Contribute"
+date: "2019-09-09"
+---
+
+Contribute motherfucker
--- src/templates/site-page.js
+++ src/templates/site-page.js
@@ -1,12 +1,25 @@
 import React from "react"
+import { graphql } from "gatsby"
 
 import Layout from "../components/layout/layout"
 
-const SitePage = () => {
+export const query = graphql`
+  query($slug: String!){
+    markdownRemark (fields: { slug: { eq: $slug } }) {
+      frontmatter {
+        title
+      }
+      html
+    }
+  }
+`
+
+const SitePage = (props) => {
   return (
     <Layout>
       <div>
-        <h1>Advertise</h1>
+        <h1>{props.data.markdownRemark.frontmatter.title}</h1>
+        <div dangerouslySetInnerHTML={{ __html: props.data.markdownRemark.html }}></div>
       </div>
     </Layout>
   )
