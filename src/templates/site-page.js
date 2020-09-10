22462b4269e1e47de747a2e202bbdfabd6ad6051
Matthew Gilligan <mcgilligan97@gmail.com>
2020-09-10 14:04:54 +0100

    Create pages for markdown posts

---
+++ src/templates/site-page.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+import Layout from "../components/layout/layout"
+
+const SitePage = () => {
+  return (
+    <Layout>
+      <div>
+        <h1>Advertise</h1>
+      </div>
+    </Layout>
+  )
+}
+
+export default SitePage
