9d81d421707a32dc40f79c0220e4a9631ca1c409
Matthew Gilligan <mcgilligan97@gmail.com>
2020-09-21 07:54:51 +0100

    Add body to review post and create head

--- src/templates/review/review.js
+++ src/templates/review/review.js
@@ -1,5 +1,6 @@
 import React from "react"
 import { graphql } from "gatsby"
+import { documentToReactComponents } from "@contentful/rich-text-react-renderer"
 
 import Layout from "../../components/layout/layout"
 import reviewStyles from "./review.module.scss"
@@ -9,16 +10,31 @@ export const query = graphql`
     contentfulReview (slug: { eq: $slug }) {
       albumTitle
       subtitle
+      body {
+        json
+      }
     }
   }
 `
 
-const SitePage = (props) => {
+const Review = (props) => {
+  const options = {
+    renderNode: {
+      "embedded-asset-block": (node) => {
+        const alt = node.data.target.fields.title['en-US']
+        const url = node.data.target.fields.file['en-US'].url
+        return <img alt={alt} src={url} />
+      }
+    }
+  }
+
   return (
     <Layout>
       <h1>{props.data.contentfulReview.albumTitle}</h1>
+      <p>{props.data.contentfulReview.subtitle}</p>
+      {documentToReactComponents(props.data.contentfulReview.body.json, options)}
     </Layout>
   )
 }
 
-export default SitePage
+export default Review
