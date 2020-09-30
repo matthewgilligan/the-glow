601bff4b756984a9333e76f16ab15c9461c32ce2
Matthew Gilligan <mcgilligan97@gmail.com>
2020-09-30 19:30:00 +0100

    Implement artist catalogue

--- src/templates/artist/artist.js
+++ src/templates/artist/artist.js
@@ -1,16 +1,64 @@
 import React from "react"
-import { graphql } from "gatsby"
+import { Link, graphql } from "gatsby"
 
 import Layout from "../../components/layout/layout"
-import artistStyles from "./artist.module.scss"
+{/*import artistStyles from "./artist.module.scss"*/}
 
 export const query = graphql`
   query($slug: String!){
-    markdownRemark (fields: { slug: { eq: $slug } }) {
-      frontmatter {
-        title
+    contentfulArtist (slug: { eq: $slug }) {
+      englishName
+      japaneseName
+    }
+    allContentfulReview (
+      filter: {
+        artist:{
+          slug: {
+            eq: $slug
+          }
+        }
       }
-      html
+    ) {
+      edges {
+        node {
+          albumTitle
+          slug
+        }
+      }
+    }
+    allContentfulNews (
+      filter: {
+        artist:{
+          elemMatch:{
+            slug: {
+              eq: $slug
+            }
+          }
+        }
+      }
+    ) {
+      edges {
+        node {
+          title
+        }
+      }
+    }
+    allContentfulFeature (
+      filter: {
+        artist:{
+          elemMatch:{
+            slug: {
+              eq: $slug
+            }
+          }
+        }
+      }
+    ) {
+      edges {
+        node {
+          title
+        }
+      }
     }
   }
 `
@@ -19,8 +67,35 @@ const Artist = (props) => {
   return (
     <Layout>
       <div>
-        <h1 className={artistStyles.title}>Hello</h1>
-        <div>Hello</div>
+        <h1>{props.data.contentfulArtist.englishName}</h1>
+        <div>{props.data.contentfulArtist.japaneseName}</div>
+        <ul>
+          {props.data.allContentfulReview.edges.map((edge) => {
+            return (
+              <li>
+                <h2>{edge.node.albumTitle}</h2>
+              </li>
+            )
+          })}
+        </ul>
+        <ul>
+          {props.data.allContentfulFeature.edges.map((edge) => {
+            return (
+              <li>
+                <h2>{edge.node.title}</h2>
+              </li>
+            )
+          })}
+        </ul>
+        <ul>
+          {props.data.allContentfulNews.edges.map((edge) => {
+            return (
+              <li>
+                <h2>{edge.node.title}</h2>
+              </li>
+            )
+          })}
+        </ul>
       </div>
     </Layout>
   )
