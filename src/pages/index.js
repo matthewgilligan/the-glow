6a057c26726eebf8a42327b16aa2949b4fa9658e
Matthew Gilligan <mcgilligan97@gmail.com>
2020-10-05 19:45:11 +0100

    Set up reviews section on home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,66 @@
 import React from "react"
+import { Link, graphql } from "gatsby"
 
 import Layout from "../components/layout/layout"
+import Head from "../components/head/head"
+import indexStyles from "./index.module.scss"
+import reviewsStyles from "./reviews.module.scss"
 
-const IndexPage = () => {
+export const query = graphql`
+  query {
+    allContentfulReview ( sort: { fields:publishedDate, order:DESC }, limit: 12 ) {
+      edges {
+        node {
+          albumTitle
+          slug
+          artist {
+            englishName
+          }
+          albumCover {
+            title
+            file {
+              url
+            }
+          }
+        }
+      }
+    }
+    allContentfulNews ( sort: { fields:publishedDate, order:DESC } ) {
+      edges {
+        node {
+          title
+          slug
+        }
+      }
+    }
+    allContentfulFeature ( sort: { fields:publishedDate, order:DESC } ) {
+      edges {
+        node {
+          title
+          slug
+        }
+      }
+    }
+  }
+`
+
+const IndexPage = (props) => {
   return (
     <Layout>
-      <div>
-        <h1>Hello</h1>
+      <div className={indexStyles.featureInterview}>
+      </div>
+      <div className={reviewsStyles.albums}>
+        {props.data.allContentfulReview.edges.map((edge) => {
+          return (
+            <div className={reviewsStyles.album}>
+              <Link to={`${edge.node.slug}`}>
+                <img src={edge.node.albumCover.file.url} alt={edge.node.albumCover.title} className={reviewsStyles.albumCover} />
+                <h2 className={reviewsStyles.artistName}>{edge.node.artist.englishName}</h2>
+                <h2 className={reviewsStyles.albumTitle}>{edge.node.albumTitle}</h2>
+              </Link>
+            </div>
+          )
+        })}
       </div>
     </Layout>
   )
