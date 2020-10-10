7faf833ac87d02a45efdf986ff73ab0f68b97dd0
Matthew Gilligan <mcgilligan97@gmail.com>
2020-10-10 19:09:46 +0100

    Add basic style to search results

--- src/components/search/hit.js
+++ src/components/search/hit.js
@@ -2,19 +2,14 @@ import React from 'react'
 import { Link } from 'gatsby'
 
 import hitStyles from './hit.module.scss'
+import reviewsStyles from '../../pages/reviews.module.scss'
 
 export default ({ hit }) => (
-  <div className={hitStyles.preview}>
-    <img src={hit.albumCover.fields.file.url} className={hitStyles.heroImage}></img>
-    <h3 className={hitStyles.previewTitle}>
-      <Link to={`/reviews/${hit.slug}`}>{hit.albumTitle}</Link>
-    </h3>
-    <small>{hit.artist.fields.englishName}</small>
-    {hit.tags &&
-      hit.tags.map(tag => (
-        <p className={hitStyles.tag} key={tag}>
-          {tag}
-        </p>
-      ))}
+  <div className={hitStyles.album}>
+    <Link to={`/reviews/${hit.slug}`}>
+      <img src={hit.albumCover.fields.file.url} alt={hit.albumCover.fields.title} className={hitStyles.heroImage} />
+      <h2 className={hitStyles.artist}>{hit.artist.fields.englishName}</h2>
+      <h2 className={reviewsStyles.albumTitle}>{hit.albumTitle}</h2>
+    </Link>
   </div>
 )
--- src/pages/search.js
+++ src/pages/search.js
@@ -8,12 +8,9 @@ import 'instantsearch.css/themes/algolia.css'
 
 class Search extends React.Component {
   render() {
-    const siteTitle = get(this, 'props.data.site.siteMetadata.title')
-
     return (
       <Layout location={this.props.location}>
         <div style={{ background: '#fff' }}>
-          <Helmet title={siteTitle} />
           <div className="hero">Blog</div>
           <div className="wrapper">
             <SearchComp />
