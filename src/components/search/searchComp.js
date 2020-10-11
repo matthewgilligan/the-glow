83aeb4186c51dd8bf2bfbee18ae745514c06cbeb
Matthew Gilligan <mcgilligan97@gmail.com>
2020-10-11 18:30:55 +0100

    Style results

--- src/components/search/searchComp.js
+++ src/components/search/searchComp.js
@@ -24,7 +24,7 @@ export default function() {
             className={searchStyles.searchBox}
             autoFocus
             translations={{
-              placeholder: 'Search here...',
+              placeholder: 'Search an artist...',
             }}
           />
         </header>
