864f40ad56afbd6eec6074d323a5184d9ca0ef84
Matthew Gilligan <mcgilligan97@gmail.com>
2020-10-14 20:52:17 +0100

    Restructure the guide template and style reccomendations

--- src/templates/guide/guide.js
+++ src/templates/guide/guide.js
@@ -3,7 +3,8 @@ import { Link, graphql } from "gatsby"
 import { documentToReactComponents } from "@contentful/rich-text-react-renderer"
 
 import Head from "../../components/head/head"
-import Layout from "../../components/layout/layout"
+import Footer from "../../components/footer/footer"
+import Header from "../../components/header/header"
 import guideStyles from "./guide.module.scss"
 
 export const query = graphql`
@@ -124,27 +125,37 @@ const Guides = (props) => {
   const reccomendations = [props.data.contentfulGuide.reference1, props.data.contentfulGuide.reference2, props.data.contentfulGuide.reference3, props.data.contentfulGuide.reference4, props.data.contentfulGuide.reference5]
 
   return (
-    <Layout>
+    <div>
       <Head title={props.data.contentfulGuide.title}/>
-      <div className={guideStyles.banner} style={{backgroundImage: `radial-gradient(rgba(0,0,0,0.4),rgba(0,0,0,0)), url(${props.data.contentfulGuide.coverImage.file.url})`}}>
-        <div className={guideStyles.bannerImage}>
-          <div className={guideStyles.bannerTitle}>
-            <h3>A Brief Guide to</h3>
-            <h1>{props.data.contentfulGuide.title}</h1>
-            <h3>by The Glow</h3>
+      <div className={guideStyles.container}>
+        <Header />
+      </div>
+
+      <div className={guideStyles.container}>
+        <div className={guideStyles.banner} style={{backgroundImage: `radial-gradient(rgba(0,0,0,0.4),rgba(0,0,0,0)), url(${props.data.contentfulGuide.coverImage.file.url})`}}>
+          <div className={guideStyles.bannerImage}>
+            <div className={guideStyles.bannerTitle}>
+              <h3>A Brief Guide to</h3>
+              <h1>{props.data.contentfulGuide.title}</h1>
+              <h3>by The Glow</h3>
+            </div>
           </div>
         </div>
       </div>
-      <div className={guideStyles.body}>
-        {documentToReactComponents(props.data.contentfulGuide.body.json, options)}
+
+      <div className={guideStyles.container}>
+        <div className={guideStyles.body}>
+          {documentToReactComponents(props.data.contentfulGuide.body.json, options)}
+        </div>
       </div>
+
       <div className={guideStyles.reccomendations}>
         <h2>Top 5 {props.data.contentfulGuide.title} Albums</h2>
         <div className={guideStyles.albums}>
           {reccomendations.map((reccomendation) => {
             return (
               <div className={guideStyles.album}>
-                <Link to={`${reccomendation.slug}`}>
+                <Link to={`../../reviews/${reccomendation.slug}`}>
                   <img src={reccomendation.albumCover.file.url} alt={reccomendation.albumCover.title} className={guideStyles.albumCover} />
                   <h2 className={guideStyles.artistName}>{reccomendation.artist.englishName}</h2>
                   <h2 className={guideStyles.albumTitle}>{reccomendation.albumTitle}</h2>
@@ -154,7 +165,11 @@ const Guides = (props) => {
           })}
         </div>
       </div>
-    </Layout>
+
+      <div className={guideStyles.container}>
+        <Footer />
+      </div>
+    </div>
   )
 }
 
