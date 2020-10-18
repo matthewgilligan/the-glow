800b3a59648b946a8490bacc5a36df809a623c5d
Matthew Gilligan <mcgilligan97@gmail.com>
2020-10-18 17:33:51 +0100

    Add category pages for news

--- src/pages/news/releases.js
+++ src/pages/news/releases.js
@@ -1,15 +1,15 @@
 import React from "react"
 import { graphql, useStaticQuery, Link } from "gatsby"
 
-import Layout from "../components/layout/layout"
-import featuresStyles from "./features.module.scss"
-import stickyNavStyles from '../components/sticky-nav/sticky-nav.module.scss'
-import Head from "../components/head/head"
+import Layout from "../../components/layout/layout"
+import featuresStyles from "../features.module.scss"
+import stickyNavStyles from '../../components/sticky-nav/sticky-nav.module.scss'
+import Head from "../../components/head/head"
 
 const FeaturesPage = () => {
   const data = useStaticQuery(graphql`
     query {
-      allContentfulNews ( sort: { fields:publishedDate, order:DESC } ) {
+      allContentfulNews ( sort: { fields:publishedDate, order:DESC }, filter: { category:{ title: { eq: "Releases" } } } ) {
         edges {
           node {
             title
@@ -56,13 +56,13 @@ const FeaturesPage = () => {
         <div className={stickyNavStyles.navItems}>
           <ul>
             <li>
-              All News
+              <Link to="../">All News</Link>
             </li>
             <li>
-              <Link to="./tours">Tours</Link>
+              <Link to="../tours">Tours</Link>
             </li>
             <li>
-              <Link to="./releases">Releases</Link>
+              Releases
             </li>
           </ul>
           <div class={stickyNavStyles.dropdown}>
@@ -80,7 +80,7 @@ const FeaturesPage = () => {
       </div>
       <div class={featuresStyles.topFeatures}>
         <div class={featuresStyles.firstFeature}>
-          <Link to={`${firstFeature.node.slug}`}>
+          <Link to={`../${firstFeature.node.slug}`}>
             <div class={featuresStyles.firstFeatureImg}
               style={{backgroundImage: `linear-gradient(rgba(0,0,0,0.0), 65%, rgba(0,0,0,0.9)), url(${firstFeature.node.coverImage.file.url})`} }>
               <div class={featuresStyles.firstFeatureDetails}>
@@ -96,7 +96,7 @@ const FeaturesPage = () => {
           {scrollFeatures.map((edge) => {
             return (
               <li className={featuresStyles.scrollFeature}>
-                <Link to={`${edge.node.slug}`}>
+                <Link to={`../${edge.node.slug}`}>
                   <div class={featuresStyles.scrollFeatureImage} style={{backgroundImage: `url(${edge.node.coverImage.file.url})`} }></div>
                   <div class={featuresStyles.scrollFeatureDetails}>
                     <p class={featuresStyles.scrollFeatureTitle}>{edge.node.title}</p>
@@ -116,7 +116,7 @@ const FeaturesPage = () => {
           return (
             <li>
               <div className={featuresStyles.remainingFeature}>
-                <Link to={`${edge.node.slug}`} class={featuresStyles.remainingFeatureImageLink}>
+                <Link to={`../${edge.node.slug}`} class={featuresStyles.remainingFeatureImageLink}>
                   <div class={featuresStyles.remainingFeatureImage} style={{backgroundImage: `url(${edge.node.coverImage.file.url})`} }></div>
                 </Link>
                 <div class={featuresStyles.remainingFeatureDetails}>
