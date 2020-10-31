e3bd35406ceea8204660142b0c930edca71504de
Matthew Gilligan <mcgilligan97@gmail.com>
2020-10-31 18:43:26 +0000

    Begin responsiveness to news template

--- src/templates/guide/guide.js
+++ src/templates/guide/guide.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { Link, graphql } from "gatsby"
+import { graphql } from "gatsby"
 import { documentToReactComponents } from "@contentful/rich-text-react-renderer"
 
 import Head from "../../components/head/head"
