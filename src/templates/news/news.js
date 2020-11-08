acf8632fbfacf14b1d39043233b6f10ef72976d6
Matthew Gilligan <mcgilligan97@gmail.com>
2020-11-08 13:32:31 +0000

    Add improved article meta section design to news

--- src/templates/news/news.js
+++ src/templates/news/news.js
@@ -1,9 +1,12 @@
 import React from "react"
 import { Link, graphql } from "gatsby"
 import { documentToReactComponents } from "@contentful/rich-text-react-renderer"
+import { ShareButtonIconOnly, ShareBlockStandard } from "react-custom-share";
+import { FaFacebookF, FaTwitter } from 'react-icons/fa'
 
 import Head from "../../components/head/head"
 import Layout from "../../components/layout/layout"
+import reviewStyles from "../review/review.module.scss"
 import featureStyles from "../feature/feature.module.scss"
 import newsStyles from "./news.module.scss"
 
@@ -49,6 +52,16 @@ const News = (props) => {
     renderText: text => text.split('\n').flatMap((text, i) => [i > 0 && <br />, text])
   }
 
+  const shareBlockProps = {
+    url: `https://xenodochial-dubinsky-db8110.netlify.app/news/${props.data.contentfulNews.slug}`,
+    button: ShareButtonIconOnly,
+    buttons: [
+      { network: "Twitter", icon: FaTwitter },
+      { network: "Facebook", icon: FaFacebookF },
+    ],
+    text: `${props.data.contentfulNews.title}`,
+  };
+
   return (
     <Layout>
       <Head title={`${props.data.contentfulNews.title} | The Glow`}/>
@@ -57,12 +70,20 @@ const News = (props) => {
         <img src={props.data.contentfulNews.coverImage.file.url} alt={props.data.contentfulNews.coverImage.title} className={newsStyles.coverImage} />
       </div>
       <div className={featureStyles.featureContent}>
-        <div className={featureStyles.authorDetails}>
-          <Link to={`../../author/${props.data.contentfulNews.author.slug}`}>
-            <p>By: {props.data.contentfulNews.author.englishName}</p>
-          </Link>
-          <p className={featureStyles.date}>{props.data.contentfulNews.publishedDate}</p>
-          <p className={featureStyles.genre}>{props.data.contentfulNews.category.title}</p>
+        <div className={reviewStyles.metaDetails}>
+          <p>By: <Link to={`../../author/${props.data.contentfulNews.author.slug}`}>{props.data.contentfulNews.author.englishName}</Link></p>
+          <p className={reviewStyles.date}>{props.data.contentfulNews.publishedDate}</p>
+          <div className={reviewStyles.genreAndSocials}>
+            <p className={reviewStyles.genre}>{props.data.contentfulNews.category.title}</p>
+            <ShareBlockStandard {...shareBlockProps} />
+          </div>
+        </div>
+        <div className={reviewStyles.mobileMetaDetails}>
+          <div className={reviewStyles.genreAndSocials}>
+            <p>By: <Link to={`../../author/${props.data.contentfulNews.author.slug}`}>{props.data.contentfulNews.author.englishName}</Link></p>
+            <p className={reviewStyles.genre}>{props.data.contentfulNews.genre.name}</p>
+          </div>
+          <p className={reviewStyles.date}>{props.data.contentfulNews.publishedDate}</p>
         </div>
         <div className={featureStyles.body}>
           <p className={featureStyles.subtitle}>{props.data.contentfulNews.subtitle}</p>
