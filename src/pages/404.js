b6f3e51f12dc5c5bba84b465d9249fc76a8c0544
Matthew Gilligan <mcgilligan97@gmail.com>
2020-11-21 16:23:01 +0000

    Add style to 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -3,14 +3,18 @@ import { Link } from "gatsby"
 
 import Layout from "../components/layout/layout"
 import Head from "../components/head/head"
+import fourOFourStyles from "./404.module.scss"
 
 const NotFound = () => {
   return (
     <Layout>
       <Head title="Page Not Found"/>
-      <div>
-        <h1>Page not found</h1>
-        <p><Link to="/">Head home</Link></p>
+      <div className={fourOFourStyles.banner}>
+        <div className={fourOFourStyles.text}>
+          <h1 className={fourOFourStyles.fourOFour}>404</h1>
+          <h2 className={fourOFourStyles.notFound}>Unfortunately, The Glow does not shine here!</h2>
+          <p>Head back to our <Link to="/">home page</Link>!</p>
+        </div>
       </div>
     </Layout>
   )
