723ce58f7613e896e176f174af4c5fddbcaab0ed
Matthew Gilligan <mcgilligan97@gmail.com>
2020-11-21 20:44:26 +0000

    Add lazy image load to sub feature indexs

--- src/pages/features/interviews.js
+++ src/pages/features/interviews.js
@@ -1,8 +1,10 @@
 import React from "react"
 import { graphql, useStaticQuery, Link } from "gatsby"
+import Img from "gatsby-image"
 
 import Layout from "../../components/layout/layout"
 import featuresStyles from "../features.module.scss"
+import newsStyles from "../news.module.scss"
 import stickyNavStyles from '../../components/sticky-nav/sticky-nav.module.scss'
 import Head from "../../components/head/head"
 
@@ -30,6 +32,9 @@ const FeaturesPage = () => {
                 url
               }
               title
+              fluid {
+                ...GatsbyContentfulFluid
+              }
             }
           }
         }
@@ -47,7 +52,7 @@ const FeaturesPage = () => {
     <div class={featuresStyles.wideScreen}>
       <div class={featuresStyles.topFeatures}>
         <div class={featuresStyles.firstFeature}>
-          <Link to={`../${firstFeature.node.slug}`}>
+          <Link to={`${firstFeature.node.slug}`}>
             <div class={featuresStyles.firstFeatureImg}
               style={{backgroundImage: `linear-gradient(rgba(0,0,0,0.0), 65%, rgba(0,0,0,0.9)), url(${firstFeature.node.coverImage.file.url})`} }>
               <div class={featuresStyles.firstFeatureDetails}>
@@ -63,13 +68,23 @@ const FeaturesPage = () => {
           {scrollFeatures.map((edge) => {
             return (
               <li className={featuresStyles.scrollFeature}>
-                <Link to={`../${edge.node.slug}`}>
-                  <div class={featuresStyles.scrollFeatureImage} style={{backgroundImage: `url(${edge.node.coverImage.file.url})`} }></div>
+                <Link to={`${edge.node.slug}`}>
+                  <Img
+                    fluid={edge.node.coverImage.fluid}
+                    key={edge.node.coverImage.fluid.src}
+                    alt={edge.node.coverImage.title}
+                    className={featuresStyles.scrollFeatureImage}>
+                  </Img>
                   <div class={featuresStyles.scrollFeatureDetails}>
                     <p class={featuresStyles.scrollFeatureTitle}>{edge.node.title}</p>
-                    <div class={featuresStyles.scrollFeatureInfo}>
-                      <p class={featuresStyles.scrollFeatureAuthor}>By: {edge.node.author.englishName}</p>
-                      <p class={featuresStyles.scrollFeatureDate}>{edge.node.publishedDate}</p>
+                    <div class={newsStyles.remainingInfo}>
+                      <div class={newsStyles.remainingMeta}>
+                        <p class={newsStyles.remainingAuthor}>By: {edge.node.author.englishName}</p>
+                        <p class={newsStyles.remainingDate}>{edge.node.publishedDate}</p>
+                      </div>
+                      <Link to={edge.node.category.slug}  className={newsStyles.remainingCategory}>
+                        <p>{edge.node.category.name}</p>
+                      </Link>
                     </div>
                   </div>
                 </Link>
@@ -83,17 +98,27 @@ const FeaturesPage = () => {
           return (
             <li>
               <div className={featuresStyles.remainingFeature}>
-                <Link to={`../${edge.node.slug}`} class={featuresStyles.remainingFeatureImageLink}>
-                  <div class={featuresStyles.remainingFeatureImage} style={{backgroundImage: `url(${edge.node.coverImage.file.url})`} }></div>
+                <Link to={`${edge.node.slug}`} class={featuresStyles.remainingFeatureImageLink}>
+                  <Img
+                    fluid={edge.node.coverImage.fluid}
+                    key={edge.node.coverImage.fluid.src}
+                    alt={edge.node.coverImage.title}
+                    className={featuresStyles.remainingFeatureImage}>
+                  </Img>
                 </Link>
                 <div class={featuresStyles.remainingFeatureDetails}>
-                  <Link to={`../${edge.node.slug}`}>
+                  <Link to={`${edge.node.slug}`}>
                     <h3 class={featuresStyles.remainingFeatureTitle}>{edge.node.title}</h3>
                   </Link>
                   <p class={featuresStyles.remainingFeatureSubtitle}>{edge.node.subtitle}</p>
-                  <div class={featuresStyles.remainingFeatureInfo}>
-                    <p class={featuresStyles.remainingFeatureAuthor}>By: {edge.node.author.englishName}</p>
-                    <p class={featuresStyles.remainingFeatureDate}>{edge.node.publishedDate}</p>
+                  <div class={newsStyles.remainingInfo}>
+                    <div class={newsStyles.remainingMeta}>
+                      <p class={newsStyles.remainingAuthor}>By: {edge.node.author.englishName}</p>
+                      <p class={newsStyles.remainingDate}>{edge.node.publishedDate}</p>
+                    </div>
+                    <Link to={edge.node.category.slug}  className={newsStyles.remainingCategory}>
+                      <p>{edge.node.category.name}</p>
+                    </Link>
                   </div>
                 </div>
               </div>
@@ -106,7 +131,7 @@ const FeaturesPage = () => {
   const narrowScreen =
     <div class={featuresStyles.narrowScreen}>
       <div class={featuresStyles.narrowTopFeature}>
-        <Link to={`../${firstFeature.node.slug}`}>
+        <Link to={`${firstFeature.node.slug}`}>
           <div class={featuresStyles.narrowTopFeatureImg}
             style={{backgroundImage: `linear-gradient(rgba(0,0,0,0.0), 65%, rgba(0,0,0,0.9)), url(${firstFeature.node.coverImage.file.url})`} }>
             <div class={featuresStyles.firstFeatureDetails}>
@@ -123,17 +148,27 @@ const FeaturesPage = () => {
           return (
             <li>
               <div className={featuresStyles.remainingFeature}>
-                <Link to={`../${edge.node.slug}`} class={featuresStyles.remainingFeatureImageLink}>
-                  <div class={featuresStyles.remainingFeatureImage} style={{backgroundImage: `url(${edge.node.coverImage.file.url})`} }></div>
+                <Link to={`${edge.node.slug}`} class={featuresStyles.remainingFeatureImageLink}>
+                  <Img
+                    fluid={edge.node.coverImage.fluid}
+                    key={edge.node.coverImage.fluid.src}
+                    alt={edge.node.coverImage.title}
+                    className={featuresStyles.remainingFeatureImage}>
+                  </Img>
                 </Link>
                 <div class={featuresStyles.remainingFeatureDetails}>
-                  <Link to={`../${edge.node.slug}`}>
+                  <Link to={`${edge.node.slug}`}>
                     <h3 class={featuresStyles.remainingFeatureTitle}>{edge.node.title}</h3>
                   </Link>
                   <p class={featuresStyles.remainingFeatureSubtitle}>{edge.node.subtitle}</p>
-                  <div class={featuresStyles.remainingFeatureInfo}>
-                    <p class={featuresStyles.remainingFeatureAuthor}>By: {edge.node.author.englishName}</p>
-                    <p class={featuresStyles.remainingFeatureDate}>{edge.node.publishedDate}</p>
+                  <div class={newsStyles.remainingInfo}>
+                    <div class={newsStyles.remainingMeta}>
+                      <p class={newsStyles.remainingAuthor}>By: {edge.node.author.englishName}</p>
+                      <p class={newsStyles.remainingDate}>{edge.node.publishedDate}</p>
+                    </div>
+                    <Link to={edge.node.category.slug}  className={newsStyles.remainingCategory}>
+                      <p>{edge.node.category.name}</p>
+                    </Link>
                   </div>
                 </div>
               </div>
@@ -143,11 +178,16 @@ const FeaturesPage = () => {
       </ul>
       <div className={featuresStyles.mobileTopFeature}>
         <div className={featuresStyles.mobileFeature}>
-          <Link to={`../${firstFeature.node.slug}`}>
-            <div class={featuresStyles.mobileFeatureImage} style={{backgroundImage: `url(${firstFeature.node.coverImage.file.url})`} }></div>
+          <Link to={`${firstFeature.node.slug}`}>
+            <Img
+              fluid={firstFeature.node.coverImage.fluid}
+              key={firstFeature.node.coverImage.fluid.src}
+              alt={firstFeature.node.coverImage.title}
+              className={featuresStyles.mobileFeatureImage}>
+            </Img>
           </Link>
           <div class={featuresStyles.mobileFeatureDetails}>
-            <Link to={`../${firstFeature.node.slug}`}>
+            <Link to={`${firstFeature.node.slug}`}>
               <h3 class={featuresStyles.mobileFeatureTitle}>{firstFeature.node.title}</h3>
             </Link>
             <div class={featuresStyles.mobileFeatureInfo}>
@@ -161,16 +201,26 @@ const FeaturesPage = () => {
         {narrowRemainingFeatures.map((edge) => {
           return (
             <div className={featuresStyles.mobileFeature}>
-              <Link to={`../${edge.node.slug}`}>
-                <div class={featuresStyles.mobileFeatureImage} style={{backgroundImage: `url(${edge.node.coverImage.file.url})`} }></div>
+              <Link to={`${edge.node.slug}`}>
+                <Img
+                  fluid={edge.node.coverImage.fluid}
+                  key={edge.node.coverImage.fluid.src}
+                  alt={edge.node.coverImage.title}
+                  className={featuresStyles.mobileFeatureImage}>
+                </Img>
               </Link>
               <div class={featuresStyles.mobileFeatureDetails}>
-                <Link to={`../${edge.node.slug}`}>
+                <Link to={`${edge.node.slug}`}>
                   <h3 class={featuresStyles.mobileFeatureTitle}>{edge.node.title}</h3>
                 </Link>
-                <div class={featuresStyles.mobileFeatureInfo}>
-                  <p class={featuresStyles.mobileFeatureAuthor}>By: {edge.node.author.englishName}</p>
-                  <p class={featuresStyles.mobileFeatureDate}>{edge.node.publishedDate}</p>
+                <div class={newsStyles.remainingInfo}>
+                  <div class={newsStyles.remainingMeta}>
+                    <p class={newsStyles.remainingAuthor}>By: {edge.node.author.englishName}</p>
+                    <p class={newsStyles.remainingDate}>{edge.node.publishedDate}</p>
+                  </div>
+                  <Link to={edge.node.category.slug}  className={newsStyles.remainingCategory}>
+                    <p>{edge.node.category.name}</p>
+                  </Link>
                 </div>
               </div>
             </div>
