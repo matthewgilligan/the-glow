15b80e05e02daae6b2659f0d48fcbc6050c2ab2f
Matthew Gilligan <mcgilligan97@gmail.com>
2020-12-08 22:43:47 +0000

    Add styled components plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,6 +30,8 @@ module.exports = {
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-sass',
     'gatsby-plugin-sharp',
+    'gatsby-plugin-styled-components',
+    'babel-plugin-styled-components',
     {
         resolve: 'gatsby-plugin-mailchimp',
         options: {
