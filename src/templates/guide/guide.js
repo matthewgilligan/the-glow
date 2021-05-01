7053514d31e51d311bac7f02180717e5219bfa84
Matthew Gilligan <mcgilligan97@gmail.com>
2021-05-01 20:23:07 +0100

    Allow for 2 or 3 sets of reccs and embeds

--- src/templates/guide/guide.js
+++ src/templates/guide/guide.js
@@ -11,7 +11,19 @@ import indexStyles from "../../pages/index.module.scss"
 import guideStyles from "./guide.module.scss"
 
 const Guides = (props) => {
-  const reccomendations = [props.data.contentfulGuide.reference1, props.data.contentfulGuide.reference2, props.data.contentfulGuide.reference3, props.data.contentfulGuide.reference4, props.data.contentfulGuide.reference5]
+  const { reference1, reference2, reference3, reference4, reference5, reference6, reference7, reference8, reference9, reference10, reference11,
+    reference12, reference13, reference14, reference15 } = props.data.contentfulGuide;
+
+  console.log(props);
+  console.log(reference6);
+
+  const reccomendations = [reference1, reference2, reference3, reference4, reference5];
+  const reccomendations2 = (reference6 != null) ? [reference6, reference7, reference8, reference9, reference10] : null;
+  const reccomendations3 = (reference11 != null) ? [reference11, reference12, reference13, reference14, reference15] : null;
+
+  console.log(reccomendations);
+  console.log(reccomendations2);
+  console.log(reccomendations3);
 
   return (
     <div className={indexStyles.indexContainter}>
@@ -46,7 +58,7 @@ const Guides = (props) => {
       </div>
 
       <div className={guideStyles.reccomendations}>
-        <h2 className={guideStyles.reccomendationsHeader}>Top 5 {props.data.contentfulGuide.subject} Albums</h2>
+        <h2 className={guideStyles.reccomendationsHeader}>Top 5 {props.data.contentfulGuide.subject2 ? props.data.contentfulGuide.subject2 : props.data.contentfulGuide.subject} Albums</h2>
         <div className={guideStyles.albums}>
           {reccomendations.map((reccomendation) => {
             return (
@@ -65,13 +77,73 @@ const Guides = (props) => {
         </div>
         <div className={guideStyles.streaming}>
           <img src={logo} alt="Logo" className={guideStyles.logo}/>
-          <p className={guideStyles.checkout}>Check out our {props.data.contentfulGuide.subject} playlist!</p>
+          <p className={guideStyles.checkout}>Check out our {props.data.contentfulGuide.subject2 ? props.data.contentfulGuide.subject2 : props.data.contentfulGuide.subject} playlist!</p>
           <div className={guideStyles.embed}>
             <iframe src={props.data.contentfulGuide.spotify} title="spotify" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
             <iframe src={props.data.contentfulGuide.appleMusic} title="apple" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
           </div>
         </div>
       </div>
+      {(reference6 != null) &&
+        <div className={guideStyles.reccomendations}>
+          <h2 className={guideStyles.reccomendationsHeader}>Top 5 {props.data.contentfulGuide.subject3} Albums</h2>
+          <div className={guideStyles.albums}>
+            {reccomendations2.map((reccomendation) => {
+              return (
+                <div className={guideStyles.album}>
+                  <Img
+                    fluid={reccomendation.albumCover.fluid}
+                    key={reccomendation.albumCover.fluid.src}
+                    alt={reccomendation.albumCover.title}
+                    className={guideStyles.albumCover}>
+                  </Img>
+                  <h2 className={guideStyles.artistName}>{reccomendation.artist}</h2>
+                  <h2 className={guideStyles.albumTitle}>{reccomendation.albumTitle}</h2>
+                </div>
+              )
+            })}
+          </div>
+          {(props.data.contentfulGuide.spotify2.length > 0) &&
+            <div className={guideStyles.streaming}>
+              <img src={logo} alt="Logo" className={guideStyles.logo}/>
+              <p className={guideStyles.checkout}>Check out our {props.data.contentfulGuide.subject3} playlist!</p>
+              <div className={guideStyles.embed}>
+                <iframe src={props.data.contentfulGuide.spotify2} title="spotify" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+                <iframe src={props.data.contentfulGuide.appleMusic2} title="apple" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+              </div>
+            </div>
+          }
+        </div>
+      }
+      {(reference11 != null) &&
+        <div className={guideStyles.reccomendations}>
+          <h2 className={guideStyles.reccomendationsHeader}>Top 5 {props.data.contentfulGuide.subject4} Albums</h2>
+          <div className={guideStyles.albums}>
+            {reccomendations3.map((reccomendation) => {
+              return (
+                <div className={guideStyles.album}>
+                  <Img
+                    fluid={reccomendation.albumCover.fluid}
+                    key={reccomendation.albumCover.fluid.src}
+                    alt={reccomendation.albumCover.title}
+                    className={guideStyles.albumCover}>
+                  </Img>
+                  <h2 className={guideStyles.artistName}>{reccomendation.artist}</h2>
+                  <h2 className={guideStyles.albumTitle}>{reccomendation.albumTitle}</h2>
+                </div>
+              )
+            })}
+          </div>
+          <div className={guideStyles.streaming}>
+            <img src={logo} alt="Logo" className={guideStyles.logo}/>
+            <p className={guideStyles.checkout}>Check out our {props.data.contentfulGuide.subject4} playlist!</p>
+            <div className={guideStyles.embed}>
+              <iframe src={props.data.contentfulGuide.spotify3} title="spotify" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+              <iframe src={props.data.contentfulGuide.appleMusic3} title="apple" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+            </div>
+          </div>
+        </div>
+      }
       <Footer />
     </div>
   )
@@ -88,6 +160,9 @@ export const query = graphql`
     contentfulGuide (slug: { eq: $slug }) {
       title
       subject
+      subject2
+      subject3
+      subject4
       description
       slug
       author {
@@ -177,8 +252,166 @@ export const query = graphql`
           }
         }
       }
+      reference6 {
+        albumTitle
+        slug
+        artist
+        albumCover {
+          title
+          file {
+            url
+          }
+          fluid {
+            ...GatsbyContentfulFluid
+          }
+        }
+      }
+      reference7 {
+        albumTitle
+        slug
+        artist
+        albumCover {
+          title
+          file {
+            url
+          }
+          fluid {
+            ...GatsbyContentfulFluid
+          }
+        }
+      }
+      reference8 {
+        albumTitle
+        slug
+        artist
+        albumCover {
+          title
+          file {
+            url
+          }
+          fluid {
+            ...GatsbyContentfulFluid
+          }
+        }
+      }
+      reference8 {
+        albumTitle
+        slug
+        artist
+        albumCover {
+          title
+          file {
+            url
+          }
+          fluid {
+            ...GatsbyContentfulFluid
+          }
+        }
+      }
+      reference9 {
+        albumTitle
+        slug
+        artist
+        albumCover {
+          title
+          file {
+            url
+          }
+          fluid {
+            ...GatsbyContentfulFluid
+          }
+        }
+      }
+      reference10 {
+        albumTitle
+        slug
+        artist
+        albumCover {
+          title
+          file {
+            url
+          }
+          fluid {
+            ...GatsbyContentfulFluid
+          }
+        }
+      }
+      reference11 {
+        albumTitle
+        slug
+        artist
+        albumCover {
+          title
+          file {
+            url
+          }
+          fluid {
+            ...GatsbyContentfulFluid
+          }
+        }
+      }
+      reference12 {
+        albumTitle
+        slug
+        artist
+        albumCover {
+          title
+          file {
+            url
+          }
+          fluid {
+            ...GatsbyContentfulFluid
+          }
+        }
+      }
+      reference13 {
+        albumTitle
+        slug
+        artist
+        albumCover {
+          title
+          file {
+            url
+          }
+          fluid {
+            ...GatsbyContentfulFluid
+          }
+        }
+      }
+      reference14 {
+        albumTitle
+        slug
+        artist
+        albumCover {
+          title
+          file {
+            url
+          }
+          fluid {
+            ...GatsbyContentfulFluid
+          }
+        }
+      }
+      reference15 {
+        albumTitle
+        slug
+        artist
+        albumCover {
+          title
+          file {
+            url
+          }
+          fluid {
+            ...GatsbyContentfulFluid
+          }
+        }
+      }
       spotify
       appleMusic
+      spotify2
+      appleMusic2
+      spotify3
+      appleMusic3
     }
   }
 `
